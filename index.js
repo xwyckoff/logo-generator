9bee89141e5b0f571d1bbe270c8e1e57aea047e1
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-04-01 17:58:32 -0400

    added max-input-length inquirer prompt to make validating text length easier

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const {Triangle, Square, Circle} = require("./lib/shapes");
 const inquirer = require("inquirer");
+const maxLengthInputPrompt = require("inquirer-maxlength-input-prompt");
 const fs = require("fs");
+
+inquirer.registerPrompt('maxlength-input', maxLengthInputPrompt);
 
 inquirer.prompt ([
     {
-        type: "input",
+        type: "maxlength-input",
         message: "Enter text for your logo (Max of three characters): ",
-        name: "text"
+        name: "text",
+        maxLength: 3
     }
 ])
 
